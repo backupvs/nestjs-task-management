cc6828dee49a9e0d3e16ea0d5c6785c269df4b74
backupvs <dmitryholubev@gmail.com>
2023-06-30 20:27:08 +0300

    Added validation and typeorm connection

--- src/tasks/dto/filter.dto.ts
+++ src/tasks/dto/filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class FilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
   search?: string;
 }
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,7 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
 import { TaskStatus } from '../task.model';
+import { IsEnum, IsOptional } from 'class-validator';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
 }
