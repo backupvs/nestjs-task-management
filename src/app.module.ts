6281bbddf8d9dfc2ed59b416eace8a969a82c1a0
backupvs <dmitryholubev@gmail.com>
2023-07-04 17:47:32 +0300

    Added ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,26 @@ import { TasksModule } from './task/task.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task-management',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      envFilePath: [`${process.env.NODE_ENV}.env`],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: configService.get('NODE_ENV') === 'development',
+        synchronize: configService.get('NODE_ENV') === 'development',
+      }),
     }),
     TasksModule,
     UserModule,
