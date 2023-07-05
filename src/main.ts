8ddea33cd0bc2a23d3ee36d7003940a58b71d2b1
backupvs <dmitryholubev@gmail.com>
2023-07-05 16:04:21 +0300

    Added logger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
+  const host = process.env.HOST || 'localhost';
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -13,6 +18,8 @@ async function bootstrap() {
     }),
   );
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3000);
+
+  await app.listen(port, host);
+  logger.log(`Listening on ${host}:${port}`);
 }
 bootstrap();
